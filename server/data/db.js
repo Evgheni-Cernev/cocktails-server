4d402f7a0770aea76d98b9596a95658c9b6be079
Evgheni Cernev <zenacernev@Zenas-MacBook-Pro.local>
2023-03-17 05:43:50 +0200

    added remote db

--- server/data/db.js
+++ server/data/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connectDb = () => mongoose.connect(`mongodb://mongo:27017/mydb`, { useNewUrlParser: true });
+const connectDb = () => mongoose.connect(`mongodb+srv://evgheni:YRxMmJb8zQBtNRZ7@cluster0.tm48f.mongodb.net/?retryWrites=true&w=majority`, {  useNewUrlParser: true, useUnifiedTopology: true,});
 
 module.exports = {
     connectDb
